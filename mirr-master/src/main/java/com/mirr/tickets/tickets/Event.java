de4b742824499f2f838e27f308b32ca3cfc3eda4
Oksana Abramova <oksana.abramova@thomsonreuters.com>
2019-02-28 17:12:02 +0200

    PartThree

---
+++ mirr-master/src/main/java/com/mirr/tickets/tickets/Event.java
@@ -0,0 +1,12 @@
+package com.mirr.tickets.tickets;
+
+public class Event {
+
+    public void getBasePrice() {
+
+    }
+
+    public void getRating() {
+
+    }
+}
---
+++ mirr-master/src/main/java/com/mirr/tickets/tickets/User.java
@@ -0,0 +1,4 @@
+package com.mirr.tickets.tickets;
+
+public class User {
+}
