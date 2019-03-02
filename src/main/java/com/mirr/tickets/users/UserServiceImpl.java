f755f2d8c47a528ab5895fc63a2e2b2c073500d4
Oleksandr Abramov <sanabramov@gmail.com>
2019-03-02 15:33:25 +0200

    code review 1

--- src/main/java/com/mirr/tickets/users/UserServiceImpl.java
+++ src/main/java/com/mirr/tickets/users/UserServiceImpl.java
@@ -13,10 +13,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto save(UserDto userDto) {
         try {
+//Todo: this should only be done if user is adding into collection, i.e. if user id is 0
             int id =
                     sortedSetUsers.last().getId() + 1;
             userDto.setId(id);
+//Todo: no add operation into sortedSetUsers
         } catch (NoSuchElementException e) {
+//Todo: No explicit initiation of user id attribute (recomended to start counting user if from 1)
+//Todo: operation of adding user into collection should be done for all cases
             sortedSetUsers.add(userDto);
         }
             return userDto;
