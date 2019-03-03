fffa48bc0d54b1ec3fb28875075c6dc966167004
Oleksandr Abramov <Oleksandr_Abramov@epam.com>
2019-03-03 13:28:58 +0200

    Implementation of UserService

--- src/main/java/com/mirr/tickets/users/UserDto.java
+++ src/main/java/com/mirr/tickets/users/UserDto.java
@@ -3,10 +3,25 @@ package com.mirr.tickets.users;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 public class UserDto {
 
     int id;
     String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto userDto = (UserDto) o;
+        return id == userDto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
 }
--- src/main/java/com/mirr/tickets/users/UserServiceImpl.java
+++ src/main/java/com/mirr/tickets/users/UserServiceImpl.java
@@ -1,48 +1,70 @@
 package com.mirr.tickets.users;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class UserServiceImpl implements UserService {
 
-    private static SortedSet<UserDto> sortedSetUsers = new TreeSet<>();
-
+    private static NavigableSet<UserDto> navigableSetUsers = new TreeSet<>(UserServiceImpl::compareById);
 
     @Override
     public UserDto save(UserDto userDto) {
-        try {
-//Todo: this should only be done if user is adding into collection, i.e. if user id is 0
-            int id =
-                    sortedSetUsers.last().getId() + 1;
-            userDto.setId(id);
-//Todo: no add operation into sortedSetUsers
-        } catch (NoSuchElementException e) {
-//Todo: No explicit initiation of user id attribute (recomended to start counting user if from 1)
-//Todo: operation of adding user into collection should be done for all cases
-            sortedSetUsers.add(userDto);
+        if (userDto.getId() == 0) {
+            try {
+                userDto.setId(navigableSetUsers.last().getId() + 1);
+            } catch (NoSuchElementException e) {
+                userDto.setId(0);
+            }
         }
-            return userDto;
-        }
+        navigableSetUsers.add(userDto);
+        return userDto;
+    }
 
     @Override
     public void remove(int id) {
-
+        UserDto userDto = new UserDto();
+        userDto.setId(id);
+        navigableSetUsers.remove(userDto);
     }
 
     @Override
     public UserDto getUserById(int id) {
-        return null;
+        UserDto userDto = new UserDto();
+        userDto.setId(id);
+        UserDto userEqualIdResult = navigableSetUsers.ceiling(userDto);
+        return userEqualIdResult;
     }
 
     @Override
     public UserDto getUserByEmail(String email) {
-        return null;
+        UserDto userDto = new UserDto();
+        userDto.setEmail(email);
+
+        List<UserDto> userSearchList = new ArrayList<>(navigableSetUsers);
+        Collections.sort(userSearchList, UserServiceImpl::compareByEmail);
+
+        int pos = Collections.binarySearch(userSearchList, userDto, UserServiceImpl::compareByEmail);
+        return userSearchList.get(pos);
+    }
+
+    private static int compareByEmail(UserDto userDto1, UserDto userDto2) {
+        if (userDto1 == userDto2) return 0;
+        if (userDto1 == null) return -1;
+        if (userDto1.getEmail() == userDto2.getEmail()) return 0;
+        if (userDto1.getEmail() == null) return -1;
+        return userDto1.getEmail().compareTo(userDto2.getEmail());
+    }
+
+    private static int compareById(UserDto userDto1, UserDto userDto2) {
+        if (userDto1 == userDto2) return -1;
+        if (userDto1 == null) return 0;
+        if (userDto1.getId() == userDto2.getId()) return 0;
+        if (userDto1.getId() < userDto2.getId()) return -1;
+        return 1;
     }
 
     @Override
     public List<UserDto> getAll() {
-        return null;
+        List<UserDto> userDtoList = new ArrayList<>(navigableSetUsers);
+        return userDtoList;
     }
 }
